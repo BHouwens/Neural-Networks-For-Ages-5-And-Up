d1abcce674631cfaf5147a41e4043872a751558f
BHouwens <zhion360@gmail.com>
2016-09-05 19:17:25 +0200

    Adding penguin goodness

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Penguin } from '../Penguin/Penguin.jsx';
 import styles from './App.pcss';
 
 export class App extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            penguinNumber: 5
+        };
+    }
+
     render() {
+        let { penguinNumber } = this.state;
+
         return (
             <div className={styles.app}>
-                hi
+                <h1 className={styles.title}>Neural Networks for Ages 5 and Up</h1>
+
+                <p>
+                    This is a baby penguin. He looks like your average baby penguin, but you'll see he has a number attached to him. In this case he is a {penguinNumber}-penguin, but you can click on his number and change it to whatever you want.
+                </p>
+                
+                <Penguin number={penguinNumber}/>
+
+                <p>
+                    Our penguin, let's call him Feature (stupid, I know, but that's his name), thinks he's pretty bad-ass but he's a baby so what does he know. He needs protection on a journey he has to make, so we're going to pair him up with some friends:
+                </p> 
             </div>
         );
     }
--- src/components/Penguin/Penguin.jsx
+++ src/components/Penguin/Penguin.jsx
@@ -2,10 +2,32 @@ import React from 'react';
 import styles from './Penguin.pcss';
 
 export class Penguin extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { number : props.number };
+        this.changeNumber = this.changeNumber.bind(this);
+    }
+
+    changeNumber(value) {
+        this.setState({ number: value });
+    }
+
     render() {
+        let { number } = this.state;
+
         return (
-            <div className={styles.penguin}>
-                
+            <div className={styles.container}>
+                <input 
+                    type="number" 
+                    className={styles.number} 
+                    value={number} 
+                    max="99"
+                    min="0"
+                    onChange={e => this.changeNumber(e.target.value) }/>
+
+                <img className={styles.penguin} src="src/images/penguin.svg" />
             </div>
         );
     }
